ff65444b0e452d18582b421902de5ea71ca3737d
nagarjundeepak <deepaknagarjun@gmail.com>
2019-07-09 22:52:40 +0530

    mongoose added and mongoose connct added in app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect("mongodb://nagarjun:oneplus5@ds349247.mlab.com:49247/graphql");
+mongoose.connection.once('open',()=>{
+    console.log('connection made to database');
+})
+
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
