6052c33ad81f6a39f6ff3fdc2db6628471cd1f82
nagarjundeepak <deepaknagarjun@gmail.com>
2019-07-11 21:27:38 +0530

    schema - backend DB final commit

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql');
 const Book = require('../models/book');
 const Author = require('../models/author');
 
-const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema,GraphQLInt,GraphQLList} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema,GraphQLInt,GraphQLList,GraphQLNonNull} = graphql;
 
 // dummy data
 /* const books = [
@@ -99,8 +99,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor:{
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent,args){
                 let author = new Author({
@@ -113,9 +113,9 @@ const Mutation = new GraphQLObjectType({
         addBook:{
             type: BookType,
             args: {
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent,args){
                 let book = new Book({
