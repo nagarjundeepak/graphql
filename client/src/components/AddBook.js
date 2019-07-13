44418565675e410c046adaba2b24277fc1d16c2c
nagarjundeepak <deepaknagarjun@gmail.com>
2019-07-13 12:16:43 +0530

    setting up data

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {graphql} from 'react-apollo';
 
 import {getAuthorsQuery} from '../queries/queries';
 
 
 function AddBook(props){
+
+    // getting data from server
     const authors = props.data.authors;
     function dd(){
         if(props.data.loading){
@@ -16,22 +18,32 @@ function AddBook(props){
             })
         }
     }
+
+    // updating local state to server
+    const [name, setName] = useState('');
+    const [genre, setGenre] = useState('');
+    const [authorId, setAuthorId] = useState('');
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(name, genre, authorId);
+    }
+
     return(
         <div>
-            <form id="add-book">
+            <form id="add-book" onSubmit={handleSubmit}>
                 <div className="field">
                     <label>Book Name:</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={(e)=>{setName(e.target.value)}}></input>
                 </div>
                 <div className="field">
                     <label>Genre:</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={(e)=>{setGenre(e.target.value)}}></input>
                 </div>
                 <div className="field">
                     <label>Author:</label>
-                    <select>
-                        <option>Select Author</option>
-                        { dd() }
+                    <select onChange={(e)=>{setAuthorId(e.target.value)}}>
+                        {dd()}
                     </select>
                 </div>
                 <button>+</button>
