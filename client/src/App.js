b3128affe954759684f76e281ef0739d7c4426f1
nagarjundeepak <deepaknagarjun@gmail.com>
2019-07-13 11:15:39 +0530

    apollo added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+//components
+import BookList  from './components/BookList';
+
+// apoolo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">        
-        <p>
-          React GraphQL MongoDB App
-        </p>
-      </header>
+    <ApolloProvider>
+    <div id="main">
+      <h1>React GraphQL MongoDB App</h1>
+      <hr/>
+      <h2>My Reading List</h2>
+      <BookList/>
     </div>
+    </ApolloProvider>
   );
 }
 
